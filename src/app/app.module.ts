2d21cbd632bf0166963845304451db660559e140
Varun Sukheja <varun.sukheja@myglamm.com>
2020-05-31 21:43:22 +0530

    added cart amount component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { StepperComponent } from './components/stepper/stepper.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
 import { ProductThumbnailComponent } from './components/product-thumbnail/product-thumbnail.component';
+import { CartAmountComponent } from './components/cart-amount/cart-amount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StepperComponent,
     CarouselComponent,
-    ProductThumbnailComponent
+    ProductThumbnailComponent,
+    CartAmountComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/cart-amount/cart-amount.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-amount',
+  templateUrl: './cart-amount.component.html',
+  styleUrls: ['./cart-amount.component.scss']
+})
+export class CartAmountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
